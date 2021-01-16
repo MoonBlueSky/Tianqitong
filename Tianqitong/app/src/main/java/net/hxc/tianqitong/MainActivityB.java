fc49f848f8c432151a99f830753d7081c91c7cd0
huanxinchao <1032867556@qq.com>
2021-01-16 12:02:50 +0800

    test

---
+++ Tianqitong/app/src/main/java/net/hxc/tianqitong/MainActivityB.java
@@ -0,0 +1,43 @@
+package net.hxc.tianqitong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+
+
+}
